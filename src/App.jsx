683c1dcc00c2f3d38b137b76232bd397f001d3d5
codeTukai <tukaighosh753@gmail.com>
2026-08-21 00:49:05 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,8 @@ import CartPanel from "./components/CartPanel";
 
 import toast, { Toaster } from "react-hot-toast";
 import { fetchDataFromApi } from "./utils/api";
-import Header from "./components/header";
+import Header from "./components/Header";
+
 
 
 // Contexts
